bf946c3909fb8ba894baeb432e804bf12c09f4d0
bogibso15 <161381348+bogibso15@users.noreply.github.com>
2024-02-28 15:12:14 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -39,7 +39,7 @@ endmodule
 // Debouncing is based on a counter. A change to any input will only be recognized once ALL inputs
 // are stable for a certain duration. This approach uses a single counter vs. a counter for each
 // bit.
-module tt_um_fpga_hdl_demo (
+module tt_um_template (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     /*   // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
