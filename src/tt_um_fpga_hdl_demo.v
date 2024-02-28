43f66363b295a2aca5caf40586cf5dd51c2192a2
bogibso15 <161381348+bogibso15@users.noreply.github.com>
2024-02-28 15:13:12 -0500

    Update tt_um_fpga_hdl_demo.v

--- src/tt_um_fpga_hdl_demo.v
+++ src/tt_um_fpga_hdl_demo.v
@@ -17,11 +17,11 @@
 module top(input logic clk, input logic reset, input logic [31:0] cyc_cnt, output logic passed, output logic failed);
    // Tiny tapeout I/O signals.
    logic [7:0] ui_in, uo_out;
-   
+   logic [7:0]uio_in,  uio_out, uio_oe;
    logic [31:0] r;
    always @(posedge clk) r <= 0;
    assign ui_in = r[7:0];
-   
+   assign uio_in = 8'b0;
    logic ena = 1'b0;
    logic rst_n = ! reset;
 
@@ -42,26 +42,26 @@ endmodule
 module tt_um_fpga_hdl_demo (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    /*   // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
+       // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    */
+    
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
     
     // Synchronize.
-    logic [9:0] inputs_ff, inputs_sync;
+    logic [17:0] inputs_ff, inputs_sync;
     always @(posedge clk) begin
-        inputs_ff <= {ui_in, ena, rst_n};
+        inputs_ff <= {ui_in, uio_in, ena, rst_n};
         inputs_sync <= inputs_ff;
     end
 
     // Debounce.
     `define DEBOUNCE_MAX_CNT 14'h3fff
-    logic [9:0] inputs_candidate, inputs_captured;
+    logic [17:0] inputs_candidate, inputs_captured;
     logic sync_rst_n = inputs_sync[0];
     logic [13:0] cnt;
     always @(posedge clk) begin
@@ -79,13 +79,13 @@ module tt_um_fpga_hdl_demo (
            inputs_captured <= inputs_candidate;
         end
     end
-    logic [7:0] clean_ui_in;
+    logic [7:0] clean_ui_in, clean_uio_in;
     logic clean_ena, clean_rst_n;
-    assign {clean_ui_in, clean_ena, clean_rst_n} = inputs_captured;
+    assign {clean_ui_in, clean_uio_in, clean_ena, clean_rst_n} = inputs_captured;
 
     my_design my_design (
         .ui_in(clean_ui_in),
-        
+        .uio_in(clean_uio_in),
         .ena(clean_ena),
         .rst_n(clean_rst_n),
         .*);
@@ -101,11 +101,11 @@ endmodule
 module my_design (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    /*   // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
+       // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    */
+    
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -445,8 +445,8 @@ logic FpgaPins_Fpga_CALC_valid_or_reset_a0,
             
                // Connect Tiny Tapeout outputs. Note that uio_ outputs are not available in the Tiny-Tapeout-3-based FPGA boards.
                //*uo_out = 8'b0;
-               
-               
+               assign uio_out = 8'b0;
+               assign uio_oe = 8'b0;
             //_\end_source
    
       // LEDs.
