34dc873ae6b9614af53967da694b3cf4284b4396
LanceLander <161381350+LanceLander@users.noreply.github.com>
2024-03-05 13:16:48 -0500

    Update mixcolumns.v
    added comments, removed unnecessary variables.

--- mixcolumns.v
+++ mixcolumns.v
@@ -13,33 +13,28 @@
 \TLV
    |mixcolumn
       @0
-         $reset = *reset;
-         $rst_low = !$reset && >>1$reset;
-         $block_in[127:0] = 128'ha7be1a6997ad739bd8c9ca451f618b61;
-         $const_matrix[127:0] = 128'h02030101010203010101020303010102;
+         $block_in[127:0] = 128'ha7be1a6997ad739bd8c9ca451f618b61; //dummy test vector
+         $const_matrix[127:0] = 128'h02030101010203010101020303010102; //constant matrix for column multiplicaiton in the form of a vector
 
          /xx[3:0]
             /yy[3:0]
-               $ss[7:0] = |mixcolumn$block_in[(#yy * 8 + #xx * 32) + 7 : (#yy * 8 + #xx * 32)];
-               $cc[7:0] = |mixcolumn$const_matrix[(#yy * 32 + #xx * 8) + 7 : (#yy * 32 + #xx * 8)];
+               $ss[7:0] = |mixcolumn$block_in[(#yy * 8 + #xx * 32) + 7 : (#yy * 8 + #xx * 32)];     //breaks the input vector and
+               $cc[7:0] = |mixcolumn$const_matrix[(#yy * 32 + #xx * 8) + 7 : (#yy * 32 + #xx * 8)]; //constant matrix into matrices
                /exp[3:0]
                   ///xx[#xx]/yy[#exp]$ss * /xx[#exp]/yy[#yy]$cc
-                  $reduce_check[7:0] = (/xx[#xx]/yy[#exp]$ss[7] == 1) && (/xx[#exp]/yy[#yy]$cc != 8'h01) ? 8'h1b : 8'h00;
-                  $three_check[7:0] = /xx[#exp]/yy[#yy]$cc == 8'h03 ? /xx[#xx]/yy[#exp]$ss : 8'h00;
-                  $op[7:0] = /xx[#exp]/yy[#yy]$cc == 8'h01 ? /xx[#xx]/yy[#exp]$ss : ((/xx[#xx]/yy[#exp]$ss << 1) ^ $three_check ^ $reduce_check);
+                  $reduce_check[7:0] = (/xx[#xx]/yy[#exp]$ss[7] == 1) && (/xx[#exp]/yy[#yy]$cc != 8'h01) ? 8'h1b : 8'h00; //check if a reduction by the irreducibly polynomial is necessary
+                  $three_check[7:0] = /xx[#exp]/yy[#yy]$cc == 8'h03 ? /xx[#xx]/yy[#exp]$ss : 8'h00; //check if a multiplication by 3 is being done
+                  $op[7:0] = /xx[#exp]/yy[#yy]$cc == 8'h01 ? /xx[#xx]/yy[#exp]$ss : ((/xx[#xx]/yy[#exp]$ss << 1) ^ $three_check ^ $reduce_check); //if 1, identity. otherwise, bitshift & other operations.
                   
-               $oo[7:0] = /exp[0]$op ^ /exp[1]$op ^ /exp[2]$op ^ /exp[3]$op;
+               $oo[7:0] = /exp[0]$op ^ /exp[1]$op ^ /exp[2]$op ^ /exp[3]$op; //xor the bytes together
                \viz_js
                   box: {width: 60, height: 60},
                   layout: "vertical",
                   render() {
                      return [new fabric.Text('$oo'.asInt().toString(16), {fill: "black"})];
                   }
-            $out_matrix[31:0] = /yy[*]$oo;
-         $block_out[127:0] = /xx[*]$out_matrix;
-         
-         $test[7:0] = 8'hbf * 8'h03;
-         $test2[7:0] = (8'hbf << 1) ^ (8'hbf);
+            $out_matrix[31:0] = /yy[*]$oo; //concat matrix rows
+         $block_out[127:0] = /xx[*]$out_matrix; //concat matrix columns
          //$block_out = /xx[*]/yy[*]$out_matrix;
 
       //...
